bb349e392fb5189d0ac8b407dac32f632f31a439
Mkmihir16 <132197042+Mkmihir16@users.noreply.github.com>
2024-02-08 00:58:13 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+let ham=document.querySelector(".ham");
+let menu=document.querySelector(".menu");
+let hide=document.querySelector(".hide").classList;
+
+function  myfun(){
+    hide.add("main2");
+}
+function myfun2(){
+    hide.remove("main2")
+}
