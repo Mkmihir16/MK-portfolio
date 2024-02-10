15d1f4ed2181c9e16f8f02421e03a2e3832d626b
Mkmihir16 <132197042+Mkmihir16@users.noreply.github.com>
2024-02-10 20:12:05 +0530

    Animations and links

--- script.js
+++ script.js
@@ -11,37 +11,37 @@ function myfun2() {
 let arr = [{
     info: "Microsoft Clone",
     img: "microsoft-new.webp",
-    github: "",
-    gitlive: ""
+    github: "https://github.com/Mkmihir16/Microsoft-Clone",
+    gitlive: "https://mkmihir16.github.io/Microsoft-Clone/"
 },
 {
     info: "Weather App",
     img: "7477790.png",
-    github: "",
-    gitlive: ""
+    github: "https://github.com/Mkmihir16/Weather-webpage",
+    gitlive: "https://mkmihir16.github.io/Weather-webpage/"
 },
 {
     info: "Tic-Tac Game", img: "tic-tac-toe-board-game-3d-illustration-png.webp",
-    github: "",
-    gitlive: ""
+    github: "https://github.com/Mkmihir16/tic-tac-toe",
+    gitlive: "https://mkmihir16.github.io/tic-tac-toe/"
 },
 {
     info: "Amazon-Clone",
     img: "9041af7b-8621-4abc-80ce-7b628707510b.png",
-    github: "",
-    gitlive: ""
+    github: "https://github.com/Mkmihir16/Amazon-Clone",
+    gitlive: "https://mkmihir16.github.io/Amazon-Clone/"
 },
 {
     info: "QR Generator",
     img: "qr.png",
-    github: "",
-    gitlive: ""
+    github: "https://github.com/Mkmihir16/QR-generator",
+    gitlive: "https://mkmihir16.github.io/QR-generator/"
 },
 {
     info: "Meme Generator",
     img: "meme.png",
-    github: "",
-    gitlive: ""
+    github: "https://github.com/Mkmihir16/Meme-Generator",
+    gitlive: "https://mkmihir16.github.io/Meme-Generator/"
 }];
 let addarr = document.querySelector(".projects");
 let html = "";
@@ -58,3 +58,74 @@ arr.forEach((item) => {
     </div>`
 })
 addarr.innerHTML = html;
+var tl=gsap.timeline();
+var tl1=gsap.timeline();
+gsap.from(".rightnav h1",{
+    y:-50,
+    scale:0.5,
+    duration:1.2,
+    scale:0.5,
+    delay:1
+})
+gsap.from(".rightnav img",{
+    scale:0,
+    // rotate:360,
+    duration:1.2,
+    delay:1
+})
+tl.from(".leftnav ul li",{
+    y:-50,
+    scale:2,
+    stagger:0.8
+})
+tl1.from("main h1",{
+scale:0,
+duration:1,
+delay:0.5
+})
+tl1.from(".absolute",{
+    x:-80,
+})
+// tl1.to(".hero1 h2",{
+//     scale:0.5,
+//     duration:1,
+//     scrollTrigger:{
+//         trigger:".hero1 h2",
+//         scroller:"body",
+//         // rotation:250,
+//         opacity:0
+        
+//     }
+// })
+gsap.from(".hero1 h2", {
+    scrollTrigger: {
+        trigger: ".hero1 h2",
+        scroller: "body"
+    },
+    y:-20,
+    scale:0.2,
+    // scale: 0.5,
+    // opacity: 0,
+    markers: true,
+    duration:1
+});
+gsap.from(".hero1para", {
+    scrollTrigger: {
+        trigger: ".hero1para",
+        scroller: "body"
+    },
+    scale: 0.2,
+    opacity: 0,
+    markers: true,
+    duration:1
+});
+gsap.from(".project-list",{
+    markers:true,
+    scrollTrigger: {
+        trigger: ".project-list",
+        scroller: "body"
+    },
+    scale:0,
+    opacity:0,
+    stagger:0.6
+})
